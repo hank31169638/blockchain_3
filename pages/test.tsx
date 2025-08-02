1fb47d14c350754cee0d272d020cbc7348b15077
hank31169638 <h31169638@gmail.com>
2025-08-02 18:53:53 +0800

    fix:deploy error

---
+++ pages/test.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function TestPage() {
+  return (
+    <div style={{
+      minHeight: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      background: '#1a2332',
+      color: '#fff'
+    }}>
+      <h1>測試頁面</h1>
+    </div>
+  );
+}
